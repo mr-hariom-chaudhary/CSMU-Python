afb14b6b71b9b1ea1f470c142ebb530af0280743
mr-hariom-chaudhary <mr.hariomchaudhary@gmail.com>
2024-03-06 10:49:15 +0530

    python

---
+++ Q3.py
@@ -0,0 +1,8 @@
+# write a python program to reverse a string
+
+string = "Hariom"
+reversed_string = ""
+for char in string:
+    reversed_string = char + reversed_string
+print("Original String : ", string)
+print("Reversed String : ", reversed_string)
